f4a32eb1ed31ac0a5180ae2434a3b7e48a724a7c
marco-bee <marco.bee@unitn.it>
2021-12-12 18:07:45 +0100

    Added data

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Number of employees in year 2016 in all the firms of the Trento district in Northern Italy.
+#'
+#' A dataset containing the number of employees in year 2016 in all the firms of the Trento district.
+#'
+#' @format A numerical vector with 183 rows and 1 column.
+#' @source \url{https://dati.trentino.it/dataset/addetti-in-imprese-attive/}
+"TN2016"
