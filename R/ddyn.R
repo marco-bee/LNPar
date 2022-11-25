880cecc475939fa78144076f30edc10e64275927
marco-bee <marco.bee@unitn.it>
2022-11-25 12:53:58 +0100

    Added intTol to ddyn.R

--- R/ddyn.R
+++ R/ddyn.R
@@ -3,13 +3,15 @@
 #' This function evaluates the density of a Lognormal-GPD dynamic mixture.
 #' @param x non-negative vector: points where the function is evaluated.
 #' @param pars (6 by 1) numerical vector: values of CA1, CA2, meanlog, sdlog, xi, beta.
+#' @param intTol non-negative scalar: threshold for stopping the computation of the integral in the normalization
+#' constant: if the integral on [n-1,n] is smaller than intTol, the approximation procedure stops.
 #' @return density of the lognormal-GPD mixture evaluated at x.
 #' @keywords dynamic mixture.
 #' @export
 #' @examples
-#' dLNPar <- ddyn(x,pars)
+#' dLNPar <- ddyn(x,pars,1e-04)
 
-ddyn <- function(x,pars)
+ddyn <- function(x,pars,intTol)
 {
 muc <- pars[1]
 tau <- pars[2]
@@ -23,7 +25,7 @@ temp <- (1-p) * dlnorm(x,mu,sigma) + p * evir::dgpd(x, xi, mu=0, beta)
 I <- NULL
 I1 <- 10
 i <- 1
-while (abs(I1) > .0001)
+while (abs(I1) > intTol)
 {
   temp1 <- pracma::quadinf(f,i-1,i)
   I1 <- temp1$Q
