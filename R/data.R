d1f853c4ebd77b17954220e51b608d332b508685
marco-bee <marco.bee@unitn.it>
2023-05-02 21:37:49 +0200

    Fixed database again

--- R/data.R
+++ R/data.R
@@ -6,12 +6,3 @@
 #' @format A numerical vector with 183 rows and 1 column.
 #' @source \url{https://dati.trentino.it/}
 "TN2016"
-
-#' Population estimate of the US metropolitan areas
-#'
-#' A dataset cotaining the 2019 population estimate, divided by 10000, of the
-#' 415 US metropolitan areas computed by the US Census Bureau.
-#'
-#' @format A numerical vector with 415 rows and 1 column.
-#' @source \url{https://www.census.gov/data/datasets/time-series/demo/popest/2010s-total-metro-and-micro-statistical-%20areas.html/}
-"Metro2019"
